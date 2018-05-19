7dffcd295f550ac808deadfbfe6eaa66b0cefbd3
jisixiao <jisixiao23@sina.com>
2018-05-19 21:25:30 +0800

    初次上传代码

---
+++ proscenium/src/main/java/com/jsz/proscenium/ProsceniumApplication.java
@@ -0,0 +1,12 @@
+package com.jsz.proscenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsceniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsceniumApplication.class, args);
+	}
+}
