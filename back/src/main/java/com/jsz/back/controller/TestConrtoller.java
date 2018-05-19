5361e254d29bb29caf399abfc7dba2fb0602612f
jisixiao <jisixiao23@sina.com>
2018-05-19 22:31:14 +0800

    初次上传代码

---
+++ back/src/main/java/com/jsz/back/controller/TestConrtoller.java
@@ -0,0 +1,24 @@
+package com.jsz.back.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author jushangzhi@novacloud.com
+ * @create 2018-05-19 17:09
+ * @description
+ **/
+@Controller
+public class TestConrtoller {
+
+    private Logger logger = LoggerFactory.getLogger(TestConrtoller.class);
+
+    @RequestMapping("/view")
+    public String view() {
+        logger.info("测试Jsp");
+        return "/index";
+    }
+
+}
